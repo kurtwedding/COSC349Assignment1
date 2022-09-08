7dcb16b93cec33f7db39b4420f128983cc60cd43
Kurt Wedding-Speight <wedku875@student.otago.ac.nz>
2022-09-08 19:49:53 +1200

    working?

--- build-clientserver-vm.sh
+++ build-clientserver-vm.sh
@@ -5,7 +5,7 @@ apt-get install -y apache2 php libapache2-mod-php php-mysql
             
 # Change VM's webserver's configuration to use shared folder.
 # (Look inside test-website.conf for specifics.)
-cp /vagrant/test-website.conf /etc/apache2/sites-available/
+cp /vagrant/client-website.conf /etc/apache2/sites-available/
 
 # activate our website configuration ...
 a2ensite test-website
---
+++ www/landlord/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML//EN">
+<HTML>
+    <HEAD>
+        <TITLE>Landlord Server</TITLE>
+        <STYLE>
+        BODY {font-family: Arial, Helvetica, sans-serif; font-size: 12pt}
+        </STYLE>
+    </HEAD>
+    <BODY>
+        <H1>Landlord Server</H1>
+        <table>
+            <tr>
+                <td>
+                    <form action="landlordserver.php" method="post">
+                        <input type="submit" name="action" value="Start Server">
+                    </form>
+                </td>
+                <td>
+                    <form action="landlordserver.php" method="post">
+                        <input type="submit" name="action" value="Stop Server">
+                    </form>
+                </td>
+            </tr>
+        </table>
+        <table>
+        <?php
+            $db_host = '192.168.56.13';
+            $db_name = 'fvision';
+            $db_user = 'landlord';
+            $db_pwd = 'landlordpassword';
+
+            $pdo_dsn = "mysql:host=$db_host;dbname=$db_name";
+
+            $pdo = new PDO($pdo_dsn, $db_user, $db_pwd);
+
+            $q = $pdo->query("SELECT * FROM properties");
+
+            while($row = $q->fetch()) {
+                echo "<tr><td>".$row["address"]."</td><td>".$row["landlord"]."</td><td>".$row["tenant"]."</td></tr>\n";
+            }
+            
+            ?>
+    </BODY>
+</HTML>
